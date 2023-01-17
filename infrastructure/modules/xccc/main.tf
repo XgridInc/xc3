1e6579f2a466413d3cba1194771629cd27cb771c
faheem-khau9 <mfaheem@xgrid.co>
2023-01-17 14:11:21 +0500

    addressing requested changes

--- infrastructure/modules/xccc/main.tf
+++ infrastructure/modules/xccc/main.tf
@@ -1,6 +1,10 @@
 # Assumption: An IAM role for EC2 might be given by the customer, 
 # so we will be making the following logic dynamic
 
+locals {
+  namespace = "XCCC"
+  key       = "xccc-key"
+}
 
 # Creating IAM Role for EC2 Instance 
 resource "aws_iam_role" "this" {
@@ -19,18 +23,17 @@ resource "aws_iam_role" "this" {
   })
   managed_policy_arns = ["arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess", "arn:aws:iam::aws:policy/AmazonEC2ReadOnlyAccess"]
   tags = {
-    Name = "XCCC-EC2-Role"
+    Name = "${local.namespace}-EC2-Role"
   }
 }
-
 
 # Creating EC2 Instance profile 
 
 resource "aws_iam_instance_profile" "this" {
-  name = "xccc-ec2-profile"
+  name = "${local.namespace}-ec2-profile"
   role = aws_iam_role.this.name
   tags = {
-    Name = "XCCC-EC2-Profile"
+    Name = "${local.namespace}-EC2-Profile"
   }
 }
 
@@ -48,16 +51,15 @@ resource "aws_instance" "this" {
   user_data = file("${path.module}/startup-script.sh")
 
   tags = {
-    Name = "XCCC-EC2"
+    Name = "${local.namespace}-EC2"
   }
 }
 
 resource "aws_key_pair" "this" {
-  key_name   = "xccc-key"
-  public_key = file("xccc-key.pub")
-  tags       = { Name = "XCCC-SSH-KEY" }
+  key_name   = local.namespace
+  public_key = file("${local.namespace}.pub")
+  tags       = { Name = "${local.namespace}-SSH-KEY" }
 }
-
 
 # Configuring SES Identity and SQS for email notifications
 
@@ -67,5 +69,5 @@ resource "aws_ses_email_identity" "this" {
 
 resource "aws_sqs_queue" "this" {
   name = var.sqs_queue_name
-  tags = { Name = "XCCC-SQS-KEY" }
+  tags = { Name = "${local.namespace}-SQS-KEY" }
 }
