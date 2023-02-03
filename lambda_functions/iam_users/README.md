6a71fdb55c9512d330549c90c01f0fdc000bde33
samanXgrid <114464405+samanXgrid@users.noreply.github.com>
2023-02-03 18:46:23 +0500

    IAM User Workflow (#45)
    * added iam-user workflow
    
    * added tag key
    
    * added feedback
    
    * added iam user workflow
    
    * added comment
    
    ---------
    
    Co-authored-by: saman batool <samanbatool@samans-MacBook-Pro.local>
    Co-authored-by: saman batool <samanbatool@192.168.10.2>

---
+++ lambda_functions/iam_users/README.md
@@ -0,0 +1,10 @@
+## IAM Users Workflow
+In this workflow we have covered following action items
+- List IAM User in AWS Account
+- List resources under ownership of specific IAM User in each AWS Region
+- Get Cumulative Cost of resources for the last 14 days. 
+- Get Total cost of services in each AWS Region for specific IAM User
+
+## IAM User Workflow Diagram
+<img width="561" alt="Screenshot 2023-01-31 at 6 06 10 PM" src="https://user-images.githubusercontent.com/114464405/216038901-cb1a34ef-0b89-4ea3-a0dc-015bf91a26ee.png">
+
---
+++ lambda_functions/iam_users/resource_mapping_to_iam_user.py
@@ -0,0 +1,83 @@
+import json
+import boto3
+import logging
+
+def lambda_handler(event, context):
+   """
+    List resources under ownership of specific IAM User.
+
+    Args:
+        IAM Users List: IAM Users.
+
+    Returns:
+        It return list of resources under ownership of specific IAM user in aws .
+
+    Raises:
+        KeyError: Raise error if cost explorer api  call not execute.
+   """
+   case_list=[]
+   # Tag key to fetch resources 
+   tag_key = 'Owner'
+   # list of specific IAM user
+   user_detail_data = ['saman-batool','muhammad-saad','faheem-khan']
+   
+   # list of specific region 
+   list_region= ['eu-west-1','ap-southeast-1','ap-northeast-1']
+   
+   for user in user_detail_data:
+       for region_name in list_region:
+           # Initializing resources list for each region
+           subset_list=[]
+           try:
+               client = boto3.client("resourcegroupstaggingapi", region_name= region_name)
+           except Exception as e:
+               logging.error("Error in initiating boto3 client: " + str(e))
+               return {
+                    'statusCode': 500,
+                    'body': json.dumps({"Error": str(e)})
+                }
+                
+           # getting resource detail using Owner tag        
+           try:
+               response = client.get_resources(TagFilters=[
+                    {
+                        'Key': tag_key,
+                        'Values': [
+                         user,
+                        ]
+                    },
+                ])
+           except Exception as e:
+               logging.error("Error getting response from resourcegroupstaggingapi: " + str(e))
+               return {
+                    'statusCode': 500,
+                    'body': json.dumps({"Error": str(e)})
+                }
+           # Creating json dict of response data from CE API     
+           response_dict = json.dumps(response)
+           parse_string = json.loads(response_dict)
+           
+           # parsing resource info
+           dict_len = len(parse_string["ResourceTagMappingList"])
+           if dict_len == 0:
+              result_list = {'User': user, 'ResourceList': [''], 'Region': region_name}
+              case_list.append(result_list)
+           else:
+                for item in parse_string["ResourceTagMappingList"]:
+                    subset = item["ResourceARN"].split(':')
+                    subset_len = len(subset)
+                    if subset_len == 6:
+                           #adding service and resource id 
+                       resource = subset[2]+":"+subset[5]
+                       subset_list.append(resource)
+                    else:
+                           #adding service and resource id 
+                        resource = subset[2]+":"+subset[5]+":"+subset[6]
+                        subset_list.append(resource)
+                # creating resource list for cost and usage api        
+                result_list = {'User': user, 'ResourceList': subset_list, 'Region': region_name}
+                case_list.append(result_list)
+   return {
+        'statusCode': 200,
+        'body': json.dumps(case_list)
+    }
