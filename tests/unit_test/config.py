98f2c31f96227482e401734cb6388d1a495ef640
Kashan Ali <kashan.ali@xgrid.co>
2023-09-23 02:12:08 +0500

    Implement Unit Testing Version 2 final
    Issue:
    	Title: Unit Test Implementation using mock
    	Link: https://github.com/XgridInc/xc3/issues/4
    
    	[PS-2]
    	- Added licence and change directory name.
    <br>
    	[PS-1]
    	- Added unit tests for the following services:
    		- Loadbalancer
    		- API Gateway
    
    <br>
    
    - [ ] Added tests that cover your change (if possible)
    - [ ] Added/modified documentation as required (such as the `README.md`, or the `docs` directory)
    - [x] Manually tested
    - [x] Every function, interface, class has a comment describing what it does and input/output parameters
    
    Signed-off-by: Kashan Ali [ kashan.ali@xgrid.co ]

---
+++ tests/unit_test/config.py
@@ -0,0 +1,75 @@
+"""
+Copyright (c) 2023, Xgrid Inc, https://xgrid.co
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+SPDX-License-Identifier: Apache-2.0
+"""
+
+import json
+import os
+import subprocess
+
+# Run `terraform plan -out=tfplan` command to generate the plan file
+os.chdir("../../infrastructure/")
+subprocess.run(["terraform", "init"])
+subprocess.run(["terraform", "validate"])
+subprocess.run(
+    ["terraform", "plan", "-var-file=terraform.auto.tfvars", "-out=tfplan"]
+)  # noqa: E501
+
+# Run `terraform show -json tfplan` command to get the plan output in JSON format    # noqa: E501
+terraform_show_output = subprocess.run(
+    ["terraform", "show", "-json", "tfplan"], capture_output=True, text=True
+).stdout
+
+# Parse the JSON output
+terraform_plan_json = json.loads(terraform_show_output)
+
+most_expensive_service = "most_expensive_service"
+list_linked_accounts = "list_linked_accounts"
+total_account_cost = "total_account_cost"
+project_spend_cost = "project_spend_cost"
+resource_list = "resource_list"
+
+
+# Env
+env = terraform_plan_json["variables"]["env"]["value"]
+os.environ["ENV"] = env
+os.system("export ENV")
+
+
+combined_list = []
+for item in terraform_plan_json["planned_values"]["root_module"][
+    "child_modules"
+]:  # noqa: E501
+    combined_list.extend(item["resources"])
+
+# Region
+region = terraform_plan_json["variables"]["region"]["value"]
+
+# Account ID
+account_id = terraform_plan_json["variables"]["account_id"]["value"]
+
+# LB
+load_balancer = None
+for data_item in combined_list:
+    if data_item["type"] == "aws_lb" and data_item["name"] == "this":
+        load_balancer = data_item["values"]["name"]
+        break
+# api gateway
+api_gateway = None
+for data_item in combined_list:
+    if data_item["type"] == "apigw" and data_item["name"] == "this":
+        api_gateway = data_item["values"]["name"]
+        break
