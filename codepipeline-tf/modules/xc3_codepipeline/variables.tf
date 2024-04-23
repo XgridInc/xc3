960febd2b85026231c91298d23647018fa8eabcf
shehroz-khan-xgrid <shehroz.khan@xgrid.co>
2024-04-23 18:24:52 +0500

    remove region variable

--- codepipeline-tf/modules/xc3_codepipeline/variables.tf
+++ codepipeline-tf/modules/xc3_codepipeline/variables.tf
@@ -128,10 +128,10 @@ variable "docker_image_used" {
   type        = string
 }
 
-variable "region" {
-  description = "Region used for pipeline testing."
-  type        = string
-}
+# variable "region" {
+#   description = "Region used for pipeline testing."
+#   type        = string
+# }
 
 variable "key" {
   description = "key used in codepipeline(state file key)."
